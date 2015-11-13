a849f9476f7e1599fe5c4bf9fdd3e53bb82bb28c
root <root@ip-172-31-41-80.us-west-2.compute.internal>
2015-11-13 23:42:14 +0000

    removed sudo

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,7 +3,7 @@
 echo "starting build"
 
 #sudo docker pull centos:6
-echo "sudo docker build -t $DOCKER_USER/$DOCKER_APP:$TDDIUM_SESSION_ID ."
-sudo docker build -t $DOCKER_USER/$DOCKER_APP:$TDDIUM_SESSION_ID .
+echo "docker build -t $DOCKER_USER/$DOCKER_APP:$TDDIUM_SESSION_ID ."
+docker build -t $DOCKER_USER/$DOCKER_APP:$TDDIUM_SESSION_ID .
 
 echo "Done with build"
